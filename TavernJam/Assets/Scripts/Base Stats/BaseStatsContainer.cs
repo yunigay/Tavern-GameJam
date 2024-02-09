9d6ef8d8e3ada9ed6510508c58fa6e5cebcea6ee
yunigay <113728545+yunigay@users.noreply.github.com>
2024-02-09 17:38:07 -0500

    small fixes

--- TavernJam/Assets/Scripts/Base Stats/BaseStatsContainer.cs
+++ TavernJam/Assets/Scripts/Base Stats/BaseStatsContainer.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Base Stats", menuName = "SO/Base Stats")]
 public class BaseStatsContainer : ScriptableObject
 {
-    public float Attack;
+    public float Damage;
+    public float AttackSpeed;
     public float MaxHealth;
+    public float CurrentHealth;
     public float Speed;
     public float dashSpeed;
     public float dashCooldown;
+    public float jumpForce;
+
 }
 
