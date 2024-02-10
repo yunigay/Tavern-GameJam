166103bc4ac3e7f43dd8681afc3e6e9b655cb64e
yunigay <113728545+yunigay@users.noreply.github.com>
2024-02-09 23:26:56 -0500

    Enemy JumChasing

--- TavernJam/Assets/Scripts/Enemies/Enemy.cs
+++ TavernJam/Assets/Scripts/Enemies/Enemy.cs
@@ -14,10 +14,11 @@ public class Enemy : MonoBehaviour
     public Vector2 canDealMeleeDamage;
     public float stopDistance = 0.1f;
     private LayerMask groundLayer;
-    bool onGround;
+    bool onGround = true;
     protected float health;
     protected bool isInRange = false;
     protected bool canMelee = true;
+    bool canJump = true;
     [SerializeField]
     private float meleePointOffset = 1.0f;
     [SerializeField]
@@ -34,39 +35,48 @@ public class Enemy : MonoBehaviour
         animator = GetComponent<Animator>();
         groundLayer = LayerMask.GetMask("Ground");
         stats.CurrentHealth = stats.MaxHealth;
-  
-        
+
+
     }
 
     protected virtual void Update()
     {
         onGround = Physics2D.OverlapCircle(transform.position, 0.8f, groundLayer);
-    
+
         if (onGround)
         {
+        MoveToPlayer();
         }
-            MoveToPlayer();
 
-        if (canMelee )
+        if (canMelee)
         {
             Melee();
         }
-    }
-
-        protected void MoveToPlayer()
-    {
-        Vector2 direction = (player.transform.position - transform.position).normalized;
-        chasingDirection = direction;
-        rb.velocity = chasingDirection * stats.Speed;
-        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-        if (distanceToPlayer <= stopDistance)
+        if (canJump)
         {
-            rb.velocity = Vector2.zero;
-            return;
+            JumpToPlayer();
         }
     }
 
-    protected void OnDeath(GameObject death) 
+    protected void MoveToPlayer()
+    {
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+
+        // Set only the horizontal component of the chasingDirection
+        chasingDirection = new Vector2(direction.x, 0f);
+
+        // Set the horizontal velocity
+        rb.velocity = chasingDirection * stats.Speed;
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer <= stopDistance)
+        {
+            // Stop horizontal movement if the enemy is close enough to the player
+            rb.velocity = new Vector2(0f, rb.velocity.y);
+        }
+    }
+
+    protected void OnDeath(GameObject death)
     {
         Destroy(death);
     }
@@ -83,27 +93,27 @@ public class Enemy : MonoBehaviour
 
     public void Melee()
     {
-        if (canMelee)
-        {
-            Vector2 position = transform.position;
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(position + chasingDirection * meleePointOffset, meleeCirlceRadius);
+        // if (canMelee)
+        // {
+        Vector2 position = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position + chasingDirection * meleePointOffset, meleeCirlceRadius);
 
-            for (int i = 0; i < colliders.Length; i++)
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].CompareTag("Player"))
             {
-                if (colliders[i].CompareTag("Player"))
+                Player pHealth = colliders[i].GetComponent<Player>();
+                if (pHealth != null)
                 {
-                    Player pHealth = colliders[i].GetComponent<Player>();
-                    if (pHealth != null)
-                    {
-                        pHealth.TakeDamage(meleeDamage);
-                        Debug.Log(pHealth.baseStats.CurrentHealth);
-                        canMelee = false;
-                    }
+                    pHealth.TakeDamage(meleeDamage);
+                    Debug.Log(pHealth.baseStats.CurrentHealth);
+                    canMelee = false;
                 }
             }
-
-            StartCoroutine(MeleeCooldown());
         }
+
+        StartCoroutine(MeleeCooldown());
+        // }
     }
 
 
@@ -121,6 +131,17 @@ public class Enemy : MonoBehaviour
             OnDeath(gameObject);
         }
     }
+
+    private void JumpToPlayer()
+    {
+        if (player.transform.position.y > (transform.position.y + 2) && onGround)
+        {
+            Vector2 jumpDirection = (player.transform.position - transform.position).normalized;
+            rb.AddForce(jumpDirection * stats.jumpForce, ForceMode2D.Impulse);
+        Debug.Log("Jumping");
+        }
+    
+    }
 }
-  
+
 
